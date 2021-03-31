b8e321390975e55f5c3801efe6e7b8c2f0ff87a5
piethrower <piethrowerchamp@gmail.com>
2021-03-31 14:31:10 -0400

    now there are more of them

--- script.py
+++ script.py
@@ -7,10 +7,12 @@ import secrets
 client = discord.Client()
 TOKEN = secrets.token
 
+owo_strings = ("owo", "uwu", "^w^", "0w0", "vwv")
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+
 
 @client.event
 async def on_message(message):
@@ -19,8 +21,8 @@ async def on_message(message):
 
     channel = message.channel
 
-    if message.content.startswith('-> owo'):
-        messages = await channel.history(limit=5).flatten()
+    if message.content.lower().startswith(tuple(owo_strings)):
+        messages = await channel.history(limit=3).flatten()
         p_message = messages[1].content
         await channel.send(owo.owo(p_message))
 
